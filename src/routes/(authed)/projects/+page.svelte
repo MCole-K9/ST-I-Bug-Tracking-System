a553f0dce55b9ffac67ac547cd8fb88ba8dba2bd
MCole-K9 <m.b.cole300@gmail.com>
2023-12-05 16:57:23 -0500

    Refactor form and sheet components

--- src/routes/(authed)/projects/+page.svelte
+++ src/routes/(authed)/projects/+page.svelte
@@ -1,78 +1,62 @@
 <script lang="ts">
 	import type { PageData } from './$types';
-    import {Input} from '$ui/input';
-    import {Button} from '$ui/button';
-	import * as Sheet from "$ui/sheet";
+	import { Input } from '$ui/input';
+	import { Button } from '$ui/button';
+	import * as Sheet from '$ui/sheet';
 	import { cn } from '$utils';
-	import * as Form from "$ui/form";
+	import * as Form from '$ui/form';
 	import { createProjectSchema } from './schema';
 	import { superForm } from 'sveltekit-superforms/client';
 
-
-
 	export let data: PageData;
 
-
 	const form = superForm(data.form, {
-		onSubmit: ()=>{
-			console.log("submit")
-		},
-
-
-
-	})
-
-
-	
+		onSubmit: () => {
+			console.log('submit');
+		}
+	});
 </script>
 
 <div class="max-w-6xl w-full mx-auto my-3 flex justify-center items-center gap-4">
 	<form class="flex-1">
-		<Input placeholder="Search"/>
+		<Input placeholder="Search" />
 	</form>
 
-	<Form.Root method="POST" action="?/createProject" {form} controlled schema={createProjectSchema} let:config >
-		<Sheet.Root>
-			<Sheet.Trigger asChild let:builder>
-			  <Button builders={[builder]} variant="default">Create Project</Button>
-			</Sheet.Trigger>
-			<Sheet.Content side="right">
-			  <Sheet.Header>
+	<Sheet.Root>
+		<Sheet.Trigger asChild let:builder>
+			<Button builders={[builder]} variant="default">Create Project</Button>
+		</Sheet.Trigger>
+		<Sheet.Content side="right">
+			<Sheet.Header>
 				<Sheet.Title>Create Project</Sheet.Title>
 				<Sheet.Description>
-				  Create a project. This where you will be able to track bugs
+					Create a project. This where you will be able to track bugs
 				</Sheet.Description>
-			  </Sheet.Header>
+			</Sheet.Header>
+			<Form.Root method="POST" action="?/createProject" {form} controlled schema={createProjectSchema} let:config>
 				<Form.Field {config} name="name">
-				  <Form.Item>
-					<Form.Label>Name</Form.Label>
-					<Form.Input />
-					<Form.Description>This is your project display name.</Form.Description>
-					<Form.Validation />
-				  </Form.Item>
+					<Form.Item>
+						<Form.Label>Name</Form.Label>
+						<Form.Input />
+						<Form.Description>This is your project display name.</Form.Description>
+						<Form.Validation />
+					</Form.Item>
 				</Form.Field>
 				<Form.Field {config} name="description">
 					<Form.Item>
-					  <Form.Label>Description</Form.Label>
-					  <Form.Textarea />
-					  <Form.Description>This is the project description, Make It Brief</Form.Description>
-					  <Form.Validation />
+						<Form.Label>Description</Form.Label>
+						<Form.Textarea />
+						<Form.Description>This is the project description, Make It Brief</Form.Description>
+						<Form.Validation />
 					</Form.Item>
-				  </Form.Field>
+				</Form.Field>
 				<Form.Button type="submit">Submit</Form.Button>
-			  <Sheet.Footer>
-				<!-- <Sheet.Close asChild let:builder>
-				  <Button builders={[builder]} type="submit">Save changes</Button>
-				</Sheet.Close> -->
-			  </Sheet.Footer>
-			</Sheet.Content>
-		  </Sheet.Root>
-		</Form.Root>
-	
-	
+			</Form.Root>
+		</Sheet.Content>
+	</Sheet.Root>
 </div>
 
-<div class={cn("h-screen w-screen flex justify-center items-center")}>
+<div class={cn('h-screen w-screen flex justify-center items-center')}>
 	<h1 class=" text-2xl">No Projects</h1>
 </div>
 
