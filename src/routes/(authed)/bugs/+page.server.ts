7d2a2b7b39bf0cdc3f4c749c16860b11e9afb7b2
MCole-K9 <m.b.cole300@gmail.com>
2023-12-06 20:34:18 -0500

    Refactor bug-related code and add BugService

--- src/routes/(authed)/bugs/+page.server.ts
+++ src/routes/(authed)/bugs/+page.server.ts
@@ -4,6 +4,8 @@ import { createBugSchema } from './schema';
 import prisma from '$server/prisma';
 import { fail } from '@sveltejs/kit';
 import type { Prisma } from '@prisma/client';
+import { Bug } from 'lucide-svelte';
+import { BugService } from './BugService.server';
 
 export const load = (async ({url, locals, depends}) => {
 
@@ -12,47 +14,8 @@ export const load = (async ({url, locals, depends}) => {
     let project_id = url.searchParams.get("project");
     let query = url.searchParams.get("q");
 
-    let options:Prisma.BugFindManyArgs = {
-        where: {
-           
-        }
-    }
+    let bugs = BugService.getBugs({query, project_id})
 
-    if(project_id){
-        options.where = {
-            project_id
-        }
-    }
-
-    if(query){
-        
-        options.where = {
-            ...options.where,
-            OR: [
-                {
-                    name: {
-                        search: query
-                    
-                    },
-                    description: {
-                        search: query
-                    },
-                    
-                }
-            ] 
-        }
-    }
-    
-    let bugs = await prisma.bug.findMany({
-        include: {
-            project: {
-                select: {
-                    name: true
-                }   
-            }
-        },
-        ...options
-    })
     return {
         bugs,
         form: superValidate({project_id: project_id ?? "", user_id: (await locals.auth.validate())?.user.userId }, createBugSchema )
@@ -71,26 +34,7 @@ export const actions: Actions = {
         if(form.valid) {
             // do something with form.data
 
-            await prisma.$transaction(async(tx) => {
-                //cant use form.data because it has a default value for priority, severity, and status if not provided 
-                //and it is required on history the schema
-
-                let bug = await tx.bug.create({
-                    data: {
-                        ...form.data,
-                    },
-                })
-        
-                await tx.bugHistory.create({
-                    data: {
-                        bug_id: bug.id,
-                        user_id: bug.user_id,
-                        status: bug.status,
-                        priority: bug.priority,
-                        severity: bug.severity,
-                    }
-                })
-            })
+          BugService.createBug(form.data)
          
         }else{
             // do something with form.errors
---
+++ src/routes/(authed)/bugs/BugService.server.ts
@@ -0,0 +1,108 @@
+
+import type { CreateBugSchemaType, UpdateBugSchemaType } from './schema';
+import prisma from '$server/prisma';
+import type { Prisma } from '@prisma/client';
+
+export class BugService {
+
+
+    static async getBugs(options?: {project_id?: string | null, query?: string | null, more?: Prisma.BugFindManyArgs }) {
+        
+        let optionsPrisma: Prisma.BugFindManyArgs = {
+            where: {
+               
+            }
+        }
+    
+        if(options?.project_id){
+            optionsPrisma.where = {
+                project_id: options.project_id
+            }
+        }
+    
+        if(options?.query){
+            
+            optionsPrisma.where = {
+                ...optionsPrisma.where,
+                OR: [
+                    {
+                        name: {
+                            search: options.query
+                        
+                        },
+                        description: {
+                            search: options.query
+                        },
+                        
+                    }
+                ] 
+            }
+        }
+        
+        let bugs = await prisma.bug.findMany({
+            ...options?.more,  // options below will override options.more
+            include: {
+                project: {
+                    select: {
+                        name: true
+                    }   
+                }
+            },
+            ...optionsPrisma,
+            
+        })
+        return bugs;
+    
+
+    }
+    static async createBug(data: CreateBugSchemaType) {
+
+        await prisma.$transaction(async(tx) => {
+            //cant use form.data because it has a default value for priority, severity, and status if not provided 
+            //and it is required on history the schema
+
+            let bug = await tx.bug.create({
+                data: {
+                    ...data,
+                },
+            })
+    
+            await tx.bugHistory.create({
+                data: {
+                    bug_id: bug.id,
+                    user_id: bug.user_id,
+                    status: bug.status,
+                    priority: bug.priority,
+                    severity: bug.severity,
+                }
+            })
+        })
+    }
+
+    static async updateBug(id: string, user_id: string ,data: UpdateBugSchemaType) {
+        await prisma.$transaction(async(tx) => {
+            //cant use form.data because it has a default value for priority, severity, and status if not provided 
+            //and it is required on history the schema
+
+            let bug = await tx.bug.update({
+                where: {
+                    id: id
+                },
+                data: {
+                    ...data,
+                },
+            })
+    
+            await tx.bugHistory.create({
+                data: {
+                    bug_id: bug.id,
+                    user_id: user_id,
+                    status: bug.status,
+                    priority: bug.priority,
+                    severity: bug.severity,
+                }
+            })
+        })
+    }
+
+}
